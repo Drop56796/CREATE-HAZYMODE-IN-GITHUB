b2a8dc28b2e1cf75ae51c009edb8d0464c19e0f4
opposition pantophobia <162717999+Drop56796@users.noreply.github.com>
2024-04-24 21:43:09 +0800

    Create GITHUB WORKLINK HAZYMPDE.lua

---
+++ GITHUB WORKLINK HAZYMPDE.lua
@@ -0,0 +1,7 @@
+]]--
+
+This mode is unstable please sure you Exeutor can exeute HAZYMODE ----------By Create in github
+
+--[[
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/HAZYMODE/HAZYMODE/HazyMode.txt"))()
